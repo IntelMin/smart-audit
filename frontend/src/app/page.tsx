b4618cba2d8713d1b7026dbc1fcba73638a3c39c
IntelMin <intelmin0915@gmail.com>
2024-02-20 20:26:37 +0530

    feat: add wallet connect provider

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   return (
-   <>
-   </>
+    <>
+      <ConnectButton />
+    </>
   );
 }
