9042dc0bdc4c5f25831493026bf5b7124e150629
IntelMin <intelmin0915@gmail.com>
2024-02-29 00:56:01 +0530

    loader ✅

--- token-audit/src/components/loadingModal/index.tsx
+++ token-audit/src/components/loadingModal/index.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 
 function UserDetails() {
   return (
-    <div className='flex flex-col text-center'>
-      <h1>Requested audit</h1>
+    <div className='flex flex-col py-3 px-5 text-center'>
+      <h1 className='text-medium'>Requested audit</h1>
       <p className='font-normal text-sm'>
-        Request Audit in progress. Thank you for your patience; we&apos;ll
-        inform you once it&apos;s done.
+        Request Audit in progress. Thank you for your patience; we'll inform you
+        once it's done.
       </p>
     </div>
   );
@@ -44,66 +44,77 @@ function Confirmation({ setLoading }: any) {
 }
 
 export default function LoadingModal({ setLoading, activeStep }: any) {
-  // const [activeStep, setActiveStep] = useState(1);//status will be used
-
   return (
-    <div className='flex inset-0 p-4  border bg-opacity-5 backdrop-blur-lg shadow-lg  inner-shadow-xl flex-col justify-center items-center text-white rounded-xl bg-[#110b0b5b] '>
-      <div className='flex space-x-4 md:flex-row flex-col  relative'>
+    <div className='relative flex flex-col items-center gap-8 bg-[#FFFFFF0D] p-6 rounded-[16px] text-center overflow-hidden text-white justify-start lg:mx-0 md:mx-28 mx-4'>
+      <div className='flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 relative items-center'>
+        {/* <div className=' absolute h-[2px] bg-white w-full left-0 right-0 top-1/2 transform md:-translate-y-1/2 translate-y-1/2'></div>
+        
+        */}
         <Image
           src='/line.svg'
           alt=''
           width={100}
           height={10}
-          className='absolute mt-2 left-9 md:flex hidden'
+          className='absolute md:flex hidden left-16 top-1/4 right-0  transform -z-10 md:-translate-y-1/2 translate-y-1/2'
         />
         <Image
           src='/line.svg'
           alt=''
-          width={100}
-          height={10}
-          className='absolute mt-2 right-16 md:flex hidden'
+          width={50}
+          height={30}
+          className='absolute md:flex hidden left-[11.5rem] top-1/4 right-0  transform -z-10 md:-translate-y-1/2 translate-y-1/2'
         />
-
-        <div className='flex md:flex-col flex-row space-y-2 items-center gap-4 '>
+        <Image
+          src='/line.svg'
+          alt=''
+          width={80}
+          height={30}
+          className='absolute flex md:hidden  right-[13.8rem] top-1/4  rotate-90 transform -z-10 md:-translate-y-1/2 translate-y-1/2'
+        />
+          <Image
+          src='/line.svg'
+          alt=''
+          width={75}
+          height={30}
+          className='absolute flex md:hidden  right-[14rem] top-[65%]  rotate-90 transform -z-10 md:-translate-y-1/2 translate-y-1/2'
+        />
+        <div className='flex md:flex-col flex-row items-center -mb-5 -ml-36 lg:ml-0 sticky gap-2 '>
           <Image
             src={activeStep >= 0 ? "/tick.svg" : "/ellipse.svg"}
-            width='20'
-            height='20'
+            width='40'
+            height='40'
             alt='icon'
           />
           <h1 className='text-sm font-semibold'>Requested Audit</h1>
         </div>
-
-        <div className='flex md:flex-col flex-row space-y-2 items-center gap-4'>
+        <div className='bg-transparent w-0.5  mr-[16rem] flex mt-20  top-0  h-10  md:hidden  z-5 static '></div>
+        <div className='flex md:flex-col flex-row items-center -ml-44 lg:ml-0 sticky gap-2'>
           <Image
             src={activeStep >= 1 ? "/tick.svg" : "/ellipse.svg"}
-            width='20'
-            height='20'
+            width='40'
+            height='40'
             alt='icon'
           />
           <h1 className='text-sm font-semibold'>AI is Auditing</h1>
         </div>
-
-        <div className='flex md:flex-col flex-row space-y-2 items-center gap-4'>
+        <div className='bg-transparent w-0.5  mr-[16rem] -my-5 -py-5 -top-1/2 h-10  md:hidden flex'></div>
+        <div className='flex md:flex-col flex-row items-center -ml-48 md:ml-0 sticky gap-2'>
           <Image
             src={activeStep >= 2 ? "/tick.svg" : "/ellipse.svg"}
-            width='20'
-            height='20'
+            width='40'
+            height='40'
             alt='icon'
           />
           <h1 className='text-sm font-semibold'>Complete</h1>
         </div>
       </div>
 
-      <div className='flex flex-col text-center px-7'>
+      <div className='flex flex-col px-7'>
         {activeStep === 0 && <UserDetails />}
         {activeStep === 1 && <Payment />}
         {activeStep === 2 && <Confirmation setLoading={setLoading} />}
-      </div>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
+
+        {/* <div className=' blur-md backdrop-blur-lg  w-full  h-5 bg-gradient-to-tr from-[#07b7ec] via-[#3f44f0]  to-[#8e35ea] '></div> */}
       </div>
     </div>
   );
